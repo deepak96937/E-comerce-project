a22b6a4e5404534ecbf83dea1a9a84a533aaee11
Your Name <your-email@example.com>
2025-03-31 15:45:37 +0530

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from "express"
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(()=>console.log("MongoDB Connected"))
+.catch((err)=>console.log("MongoDB Connection Error"))
 
 const app = express();
 
