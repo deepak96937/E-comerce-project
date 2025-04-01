84328acced645882eec80288e120f83515f2574d
Your Name <your-email@example.com>
2025-04-01 18:56:17 +0530

    create signup routes

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
 
 dotenv.config();
 
@@ -10,10 +11,13 @@ mongoose.connect(process.env.MONGO)
     .catch((err) => console.log("MongoDB Connection Error"))
 
 const app = express();
+app.use(express.json());
+
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 })
 
 
-app.use("/api/user", userRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
