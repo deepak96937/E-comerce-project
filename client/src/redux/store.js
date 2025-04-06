b27856ef1047a86404606e090373eea71c66109d
Your Name <your-email@example.com>
2025-04-06 19:07:32 +0530

    add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,51 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './user/userSlice';
+// import { combineReducers, configureStore } from "@reduxjs/toolkit";
+// import userReducer from "./user/userSlice";
+// import {persistReducer} from "redux-persist"
+// import storage from "redux-persist/lib/storage"
+// import persistStore from "redux-persist/es/persistStore";
 
-const store = configureStore({
+// const rootReducer = combineReducers({user:userReducer})
+
+// const persistConfig = {
+//     key:"root",
+//     storage,
+//     version:1
+// }
+
+// const persistReducer = persistReducer(persistConfig, rootReducer)
+
+// const store = configureStore({
+//     reducer: {
+//         user: persistReducer
+//     },
+//     middleware: (getDefaultMiddleware) =>
+//         getDefaultMiddleware({
+//             serializableCheck: false,
+//         }),
+// });
+
+// export const persistor = persistStore(store)
+
+
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice";
+import { persistReducer as persistReducerFunction } from "redux-persist"; // ✅ alias to avoid conflict
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+    key: "root",
+    storage,
+    version: 1,
+};
+
+const persistReducer = persistReducerFunction(persistConfig, rootReducer); // ✅ uses alias
+
+export const store = configureStore({
     reducer: {
-        user: userReducer,
+        user: persistReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
@@ -11,4 +53,6 @@ const store = configureStore({
         }),
 });
 
-export default store;
+export const persistor = persistStore(store);
+
+
