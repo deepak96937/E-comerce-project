d77d4235049be3a5bd30da58a3bc11c6022fa3fa
Your Name <your-email@example.com>
2025-04-17 17:11:54 +0530

    Add contact landlord functionaltiy to the listing page

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { verifyToken } from "../utils/verifyUser.js";
-import { deleteUser, getUserListings, test, updateUser } from "../controllers/user.controller.js";
+import { deleteUser, getUser, getUserListings, test, updateUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
@@ -8,5 +8,5 @@ router.get("/test", test )
 router.post("/update/:id", verifyToken, updateUser)
 router.delete("/delete/:id", verifyToken, deleteUser)
 router.get('/listings/:id', verifyToken, getUserListings)
-
+router.get("/:id", verifyToken, getUser)
 export default router;
